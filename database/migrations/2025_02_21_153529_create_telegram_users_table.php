484045b78c298ef444e7d395656240475a405390
Taha-Shaban-Kaamel <tahashaban743@gmail.com>
2025-02-22 15:34:36 +0200

    telegram user migration
    telegram user migration

---
+++ database/migrations/2025_02_21_153529_create_telegram_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('telegram_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name')->nullbale();
+            $table->string('telegram_name');
+            $table->string('telegram_id');
+            $table->boolean('is_employee')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('telegram_users');
+    }
+};
