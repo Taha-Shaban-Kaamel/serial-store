20e5276d44576ac67a1e1a4e5a7a39f4e4d11541
Taha-Shaban-Kaamel <tahashaban743@gmail.com>
2025-02-22 15:35:20 +0200

    telegram use api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TelegramUserController;
 
 
 Route::resource('user', UserController::class)->middleware('auth:sanctum');
@@ -12,10 +13,4 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('roles', [UserController::class, 'role'])->middleware(['auth:sanctum', 'abilities:full access']);
 Route::post('assign-role', [UserController::class, 'assignRole'])->middleware(['auth:sanctum', 'abilities:full access']);
-
-
-
-// http://127.0.0.1:8000/api/login
-
-
-
+Route::resource('telegram-user', TelegramUserController::class)->middleware('auth:sanctum');
